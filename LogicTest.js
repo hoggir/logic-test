d7b1126dd3f77badf23376df6bd4de615a4c0fc2
Fadel Giras <fadelpopeye@gmail.com>
2022-04-28 16:40:58 +0700

    first commit

---
+++ LogicTest.js
@@ -0,0 +1,55 @@
+//2
+function Reverse(huruf) {
+  var curString = huruf;
+  var newString = "";
+
+  for (let i = huruf.length - 1; i >= 0; i--) {
+    newString = newString + curString[i];
+  }
+
+  return newString;
+}
+
+conaole.log(Reverse("abcde"));
+
+//4
+let a = 3;
+let b = 7;
+[a, b] = [b, a]
+
+//5
+function noEmpat(angka) {
+  var curAngka = angka;
+  var newString = "";
+
+  if (curAngka == 4) {
+    newString = "empat";
+    console.log(newString);
+  } else if (curAngka == 20) {
+    newString = "dua puluh";
+    console.log(newString);
+  } else if (curAngka == 39) {
+    newString = "tiga puluh sembilan";
+    console.log(newString);
+  } else if (curAngka == 104) {
+    newString = "silahkan masukkan bilangan 1-100";
+    console.log(newString);
+  }
+
+  return newString;
+}
+
+const angka = prompt("Masukkan angka: ");
+noEmpat(angka);
+
+//10
+function tahunKabisat(tahun) {
+  if ((0 == tahun % 4 && 0 != tahun % 100) || 0 == tahun % 400) {
+    console.log(tahun + " adalah tahun kabisat");
+  } else {
+    console.log(tahun + " bukan tahun kabisat");
+  }
+}
+
+const tahun = prompt("Masukkan tahun: ");
+tahunKabisat(tahun);
