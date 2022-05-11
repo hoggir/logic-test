e942ef3683f44fd977c98b765ab3824e53887e0e
Fadel Giras <fadelpopeye@gmail.com>
2022-05-12 05:28:34 +0700

    revisi

--- LogicTest.js
+++ LogicTest.js
@@ -1,3 +1,16 @@
+//1
+function noSatu(angka) {
+  var kali = 1;
+  for (i=angka; i>0; i--){
+      kali *= i;
+  }
+  alert(kali);
+  return kali;
+}
+
+const angka = prompt("Masukkan angka: ");
+noSatu(angka);
+
 //2
 function Reverse(huruf) {
   var curString = huruf;
@@ -10,44 +23,89 @@ function Reverse(huruf) {
   return newString;
 }
 
-console.log(Reverse("abcde"));
+const huruf = prompt("Masukkan huruf: ");
+Reverse(huruf);
+alert(Reverse(huruf));
 
 //4
-let a = 3;
-let b = 7;
-[a, b] = [b, a]
-
-//5
-function noEmpat(angka) {
-  var curAngka = angka;
-  var newString = "";
-
-  if (curAngka == 4) {
-    newString = "empat";
-    console.log(newString);
-  } else if (curAngka == 20) {
-    newString = "dua puluh";
-    console.log(newString);
-  } else if (curAngka == 39) {
-    newString = "tiga puluh sembilan";
-    console.log(newString);
-  } else if (curAngka == 104) {
-    newString = "silahkan masukkan bilangan 1-100";
-    console.log(newString);
-  }
-
-  return newString;
+function swap() {
+    
+  [a, b] = [b, a];
+  
+  console.log(`Variable a setelah di swap: ${a}`);
+  console.log(`Variable b setelah di swap: ${b}`);
 }
 
-const angka = prompt("Masukkan angka: ");
-noEmpat(angka);
+let a = prompt('masukkan variable a: ');
+let b = prompt('masukkan variable b: ');
+swap();
+
+//5
+function noEmpat(nilai){
+  var simpanNilaiBagi=0;
+  var huruf = ["", "Satu", "Dua", "Tiga", "Empat", "Lima", "Enam", "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas"];
+  var temp="";
+
+  if (nilai < 12) {
+      temp = " "+huruf[nilai];
+  }
+  else if (nilai <20) {
+      temp = noEmpat(nilai - 10) + " Belas";
+  }
+  else if (nilai < 100) {
+      simpanNilaiBagi = Math.floor(nilai/10);
+      temp = noEmpat(simpanNilaiBagi)+" Puluh"+ noEmpat(nilai % 10);
+  }
+  else if (nilai == 100) {
+      temp = "Seratus";
+  } else {
+      temp = "silahkan masukkan bilangan 1-100"
+  }
+
+  return temp;
+}
+
+const nilai = prompt("Masukkan angka: ");
+noEmpat(nilai);
+alert(noEmpat(nilai));
+
+//9
+function noSembilan(value) {
+  var angka = value;
+  var terbesar = angka[0];
+  var terkecil = angka[0];
+  
+  for (var i = 0; i < angka.length; i++) {
+      if(angka[i] < terkecil) {
+          terkecil = angka[i];
+      }
+  }
+  
+  for( i = 0; i < angka.length; i++ ){
+      if(angka[i] > terbesar ) {
+          terbesar = angka[i];
+      }
+  }
+  
+  console.log('min: ' + terkecil + ' max: ' + terbesar)
+}
+
+var a = prompt("Angka pertama: ");
+var b = prompt("Angka kedua: ");
+var c = prompt("Angka ketiga: ");
+var d = prompt("Angka keempat: ");
+var value = [a, b, c, d ];
+
+noSembilan(value)
 
 //10
 function tahunKabisat(tahun) {
   if ((0 == tahun % 4 && 0 != tahun % 100) || 0 == tahun % 400) {
     console.log(tahun + " adalah tahun kabisat");
+    alert(tahun + " adalah tahun kabisat")
   } else {
     console.log(tahun + " bukan tahun kabisat");
+    alert(tahun + " bukan tahun kabisat")
   }
 }
 
